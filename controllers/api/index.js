2bae5a22ec8c1d0ef4449a8f56dfc7cc1b30f429
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-10-04 21:22:13 -0600

    added route structure

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const blogPostRoutes = require("./blogPostRoutes");
+const commentRoutes = require("./commentRoutes");
+const userRoutes = require("./userRoutes");
+
+router.use("/blog-post", blogPostRoutes);
+router.use("/comments", commentRoutes);
+router.use("/users", userRoutes);
+
+module.exports = router;
